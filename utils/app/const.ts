3395d2e9ede9280cb9134ff860344e2d047175e0
liangshaojiang <920999181@qq.com>
2023-03-29 13:25:59 +0800

    123456

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,1 +1,5 @@
-export const DEFAULT_SYSTEM_PROMPT = "ChatGPT，一个由OpenAI训练的大型语言模型。";
+export const DEFAULT_SYSTEM_PROMPT =
+  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+
+export const OPENAI_API_HOST =
+  process.env.OPENAI_API_HOST || 'https://api.openai.com';
